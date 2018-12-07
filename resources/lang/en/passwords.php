d05a8dde351c37af5936836ad36fff1468962fd4
hpieni <hpienizzio@gmail.com>
2018-12-07 11:52:40 -0300

    primer commit

---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'El Password debe poseer al menos 6 caracteres y coincidir con el Password de confirmación.',
+    'reset' => 'Su password ha sido reestablecido!',
+    'sent' => 'El link para reestablecer el password ha sido enviado a su correo!',
+    'token' => 'El token utilizado para reestablecer el password no es válido.',
+    'user' => "No existe un usuario con el Email ingresado.",
+
+];
